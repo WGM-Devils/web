3dcaaf5d8607f7dd68333cacd99de29e978fb8cc
Leon™ <leontm.dev@gmail.com>
2023-09-09 20:11:19 +0200

    Finished the set users route

--- routes/api/users/set.js
+++ routes/api/users/set.js
@@ -3,6 +3,33 @@
 const router = require("express").Router();
 const env = require("dotenv").config();
 const fs = require("fs-extra");
+const { e200, e201, e204, e400, e401 } = require("../api");
+const randomString = require("random-string");
+
+// Code
+
+router.post("/new", (req, res, next) => {
+    if (req.headers["authorization"] !== "") {
+        if (req.headers["authorization"] === process.env.KEY) {
+            let all = fs.readJsonSync("../../../data/users.json");
+            let id = randomString({length: 30, numeric: true, letters: false, special: false});
+            while (all[id] != undefined) {
+                id = randomString({length: 30, numeric: true, letters: false, special: false});
+            };
+            let user = req.body;
+            user.createdAt = new Date().toLocaleString("de-DE", {timeZone: "UTC"});
+            user.friends = [];
+            user.lastUpdated = new Date().toLocaleString("de-DE", {timeZone: "UTC"});
+            all[id] = user;
+            fs.writeJsonSync("../../../data/users.json", all, {spaces: 4});
+            res.status(200).json(user);
+        } else {
+            res.status(401).json(e401);
+        }
+    } else {
+        res.status(400).json(e400);
+    }
+})
 
 // Exports
 
