118058934c6ad8e98f1bd903308fc382885fba58
Leon™ <leontm.dev@gmail.com>
2023-09-09 20:14:41 +0200

    Finished get route for users

--- routes/api/users/get.js
+++ routes/api/users/get.js
@@ -3,6 +3,26 @@
 const router = require("express").Router();
 const env = require("dotenv").config();
 const fs = require("fs-extra");
+const { e200, e201, e204, e400, e401, e404 } = require("../api");
+
+// Code
+
+router.post("/get?user=:id", (req, res, next) => {
+    if (req.headers["authorization"] !== "") {
+        if (req.headers["authorization"] === process.env.KEY) {
+            let all = fs.readJsonSync("../../../data/users.json");
+            if (all[req.params.id] != undefined) {
+                res.status(200).json(all[req.params.id]);
+            } else {
+                res.status(404).json(e404);
+            }
+        } else {
+            res.status(401).json(e401);
+        }
+    } else {
+        res.status(400).json(e400);
+    }
+})
 
 // Exports
 
