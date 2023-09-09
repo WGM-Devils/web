95eed8a670745dfdf4507ba15a3d0f8059d8856d
Leon™ <leontm.dev@gmail.com>
2023-09-09 20:32:38 +0200

    Added all users routes to api

--- routes/api/api.js
+++ routes/api/api.js
@@ -6,39 +6,58 @@ const env = require("dotenv").config();
 // Errors
 
 const e200 = {
-    code: 200,
-    status: "OK",
-    message: "Success!"
-}
+  code: 200,
+  status: "OK",
+  message: "Success!",
+};
 const e201 = {
-    code: 201,
-    status: "Created",
-    message: "Successfully created the requested resource!"
-}
+  code: 201,
+  status: "Created",
+  message: "Successfully created the requested resource!",
+};
 const e204 = {
-    code: 204,
-    status: "No content",
-    message: "There is no content!"
-}
+  code: 204,
+  status: "No content",
+  message: "There is no content!",
+};
 const e400 = {
-    code: 400,
-    status: "Bad Request",
-    message: "The request has no authorization header!"
-}
+  code: 400,
+  status: "Bad Request",
+  message: "The request has no authorization header!",
+};
 const e401 = {
-    code: 401,
-    status: "Unauthorized",
-    message: "Check the authorization header!"
+  code: 401,
+  status: "Unauthorized",
+  message: "Check the authorization header!",
 };
 const e404 = {
-    code: 404,
-    status: "Not found",
-    message: "The requested resource does not exist in our data!"
-}
+  code: 404,
+  status: "Not found",
+  message: "The requested resource does not exist in our data!",
+};
+
+// Code
+
+const ALL = require("./users/all");
+const DELETE = require("./users/delete");
+const SET = require("./users/set");
+const EDIT = require("./users/edit");
+const GET = require("./users/get");
+
+router.use("/users", ALL);
+router.use("/users", DELETE);
+router.use("/users", SET);
+router.use("/users", EDIT);
+router.use("/users", GET);
 
 // Exports
 
 module.exports = router;
 module.exports = {
-    e200, e201, e204, e400, e401, e404
-}
+  e200,
+  e201,
+  e204,
+  e400,
+  e401,
+  e404,
+};
