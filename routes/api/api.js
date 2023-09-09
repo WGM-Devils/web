80427fa91e65e233db3ca43f7d9440d0ca1de3af
Leon™ <leontm.dev@gmail.com>
2023-09-09 19:58:11 +0200

    Added new error codes

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,6 +3,22 @@
 const router = require("express").Router();
 const env = require("dotenv").config();
 
+// Errors
+
+const e400 = {
+    code: 400,
+    error: "Bad Request",
+    message: "The request has no authorization header!"
+}
+const e401 = {
+    code: 401,
+    error: "Unauthorized",
+    message: "Check the authorization header!"
+};
+
 // Exports
 
 module.exports = router;
+module.exports = {
+    e400, e401
+}
