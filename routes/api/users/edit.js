7a52e5b6347d8755363f918f43afc0f109aacf53
Leon™ <leontm.dev@gmail.com>
2023-09-09 20:24:46 +0200

    Changed request method

--- routes/api/users/edit.js
+++ routes/api/users/edit.js
@@ -7,26 +7,28 @@ const { e200, e201, e204, e400, e401, e404 } = require("../api");
 
 // Code
 
-router.post("/edit?user=:id", (req, res, next) => {
-    if (req.headers["authorization"] !== "") {
-        if (req.headers["authorization"] === process.env.KEY) {
-            let all = fs.readJsonSync("../../../data/users.json");
-            if (all[req.params.id] != undefined) {
-                let user = req.body;
-                user.lastUpdated = new Date().toLocaleString("de-DE", {timeZone: "UTC"});
-                all[req.params.id] = user;
-                fs.writeJsonSync("../../../data/users.json", all, {spaces: 4});
-                res.status(200).json(user);
-            } else {
-                res.status(404).json(e404);
-            }
-        } else {
-            res.status(401).json(e401);
-        }
+router.patch("/edit?user=:id", (req, res, next) => {
+  if (req.headers["authorization"] !== "") {
+    if (req.headers["authorization"] === process.env.KEY) {
+      let all = fs.readJsonSync("../../../data/users.json");
+      if (all[req.params.id] != undefined) {
+        let user = req.body;
+        user.lastUpdated = new Date().toLocaleString("de-DE", {
+          timeZone: "UTC",
+        });
+        all[req.params.id] = user;
+        fs.writeJsonSync("../../../data/users.json", all, { spaces: 4 });
+        res.status(200).json(user);
+      } else {
+        res.status(404).json(e404);
+      }
     } else {
-        res.status(400).json(e400);
+      res.status(401).json(e401);
     }
-})
+  } else {
+    res.status(400).json(e400);
+  }
+});
 
 // Exports
 
