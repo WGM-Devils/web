535ea27db44875aa7410837d0c6422c16090be35
Leon™ <leontm.dev@gmail.com>
2023-09-09 20:32:21 +0200

    Changed request method

--- routes/api/users/get.js
+++ routes/api/users/get.js
@@ -7,22 +7,22 @@ const { e200, e201, e204, e400, e401, e404 } = require("../api");
 
 // Code
 
-router.post("/get?user=:id", (req, res, next) => {
-    if (req.headers["authorization"] !== "") {
-        if (req.headers["authorization"] === process.env.KEY) {
-            let all = fs.readJsonSync("../../../data/users.json");
-            if (all[req.params.id] != undefined) {
-                res.status(200).json(all[req.params.id]);
-            } else {
-                res.status(404).json(e404);
-            }
-        } else {
-            res.status(401).json(e401);
-        }
+router.get("/get?user=:id", (req, res, next) => {
+  if (req.headers["authorization"] !== "") {
+    if (req.headers["authorization"] === process.env.KEY) {
+      let all = fs.readJsonSync("../../../data/users.json");
+      if (all[req.params.id] != undefined) {
+        res.status(200).json(all[req.params.id]);
+      } else {
+        res.status(404).json(e404);
+      }
     } else {
-        res.status(400).json(e400);
+      res.status(401).json(e401);
     }
-})
+  } else {
+    res.status(400).json(e400);
+  }
+});
 
 // Exports
 
