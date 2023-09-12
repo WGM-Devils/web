87d25e9946091003b7770b7886f1a0f51de96ed7
Leon™ <leontm.dev@gmail.com>
2023-09-12 19:24:44 +0200

    Added 404 error route

--- routes/api/api.js
+++ routes/api/api.js
@@ -63,6 +63,27 @@ router.use("/posts/comments", require("./posts/comment/add"));
 router.use("/posts/comments", require("./posts/comment/delete"));
 router.use("/posts/comments", require("./posts/comment/edit"));
 
+// 404
+
+router.use(function (req, res) {
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts("html")) {
+    res.render("404", { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts("json")) {
+    res.json({ error: "Not found" });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type("txt").send("Not found");
+});
+
 // Exports
 
 module.exports = router;
