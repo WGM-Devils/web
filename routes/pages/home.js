dcdfe9670fb7847fe0850da5a5f6992c0743a092
Leon™ <leontm.dev@gmail.com>
2023-10-01 19:51:54 +0200

    Removed useless imports

--- routes/pages/home.js
+++ routes/pages/home.js
@@ -1,9 +1,6 @@
 // Imports
 
 const router = require("express").Router();
-const env = require("dotenv").config();
-const fs = require("fs-extra");
-const { e400, e401 } = require("../api/api");
 
 // Code
 
