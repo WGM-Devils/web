3b4070b0302669c7ea74c1ab026409016d07b374
Leon™ <83974950+leontm-dev@users.noreply.github.com>
2023-11-01 15:07:00 +0100

    Fixed imports

--- routes/api/posts/send.js
+++ routes/api/posts/send.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 const env = require("dotenv").config();
 const fs = require("fs-extra");
 const { e400, e401 } = require("../api");
+const randomString = require("random-string");
 
 // Code
 
